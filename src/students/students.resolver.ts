cf1153ef218e0a2de88253df778a559b2e1ccca5
longnh <longnh.uit@gmail.com>
2024-12-30 08:54:53 +0700

    feat: create-findOne findAll students

--- src/students/students.resolver.ts
+++ src/students/students.resolver.ts
@@ -1,8 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentsService } from './students.service';
 import { StudentEntity } from './entities/student.entity';
+import { CreateStudentInput } from './dto/create-student.input';
 
 @Resolver(() => StudentEntity)
 export class StudentsResolver {
   constructor(private readonly studentsService: StudentsService) {}
+
+  @Mutation(() => StudentEntity)
+  createStudent(@Args('createStudentInput') createStudentInput: CreateStudentInput) {
+    return this.studentsService.createStudent(createStudentInput);
+  }
+
+  @Query(() => StudentEntity)
+  findOneStudent(@Args('id') id: string) {
+    return this.studentsService.findOneStudent(id);
+  }
+
+  @Query(() => [StudentEntity])
+  findAllStudent() {
+    return this.studentsService.findAllStudent();
+  }
 }
